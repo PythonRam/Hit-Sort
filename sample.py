cc86f7cedf7b29749d3267238ff67295f7cb5c1e
PythonRam <python.hithesh@gmail.com>
2016-07-26 23:01:19 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,8 +1,7 @@
 import hitsort
 import random
 array=[random.randint(0,1000) for i in range(0,75)]
-array1=array
-array1.sort()
-if(hitsort.doSort(array)==array1):
-	print "Works bitch"
+SortedArray=hitsort.doSort(array)
+print SortedArray
+
 
